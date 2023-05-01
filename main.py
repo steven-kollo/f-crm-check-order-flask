a1b4e19d05bac0eb1d150c950a7cdf28bf8098ce
Steven <steven.kollo.workspace@gmail.com>
2023-05-01 17:36:03 -0400

    frontend

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+
+
+@app.route('/find/<string:id>&<string:phone>&<string:email>')
+def find(id, phone, email):
+    print(id)
+    return {
+        'id': id,
+        'phone': phone,
+        'email': email
+    }
 
 
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
